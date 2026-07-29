632f9c6e0a2e5cca6d4ab83e58b1c29caf023fb6
Shivam <shivamgarade05@gmail.com>
2026-07-29 21:55:33 +0530

    real world example of parameter on method signiture

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
-function pair<T, U>(first: T, second: U): [T, U] {
-  return [first, second];
+interface Database {
+  get<T>(table: string): T;
 }
 
-const result = pair("ID", 101); // Inferred as [string, number]
-console.log(result)
+class MyDatabase implements Database {
+  get<T>(table: string): T {
+    // Imagine this data comes from a real database
+    const database = {
+      user: { id: 1, name: "Shivam" },
+      product: { id: 101, title: "Laptop", price: 50000 },
+    };
+
+    return database[table as keyof typeof database] as T;
+  }
+}
+
+const db = new MyDatabase();
+
+const user = db.get<{ id: number; name: string }>("user");
+const product = db.get<{ id: number; title: string; price: number }>("product");
+
+console.log(user.id,user.name);      // Shivam
+console.log(product.title,product.price);  // 50000
+
+
+// conslusion
+// We provide the type of the value we expect the method to return.
